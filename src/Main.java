5cd3dc44879ba8a236524f932cbc8b3c82f7136f
profitweb <chezsimo@yandex.u>
2023-01-09 03:05:37 +0300

    first lunch to repository

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double d ;
+        double e ;
+        System.out.println("Please enter the sum in rouble");
+        System.out.println("the sum coverted in dollar is " + ( d = 120 / 60) + " Dollars");
+        System.out.println("the sum coverted in euro is " + (e = 120 / 65)+ " Euros and " + (120 % 65) + " roubles" ) ;
+    }
+}
